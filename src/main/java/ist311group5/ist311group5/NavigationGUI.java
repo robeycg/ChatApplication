06738f129aa5dc484f5886d4f05a9812af088876
Bryce Ciemiewicz <bzc5373@psu.edu>
2021-03-12 10:13:53 -0500

    - Added chat and settings buttons to navigation GUI

--- src/main/java/ist311group5/ist311group5/NavigationGUI.java
+++ src/main/java/ist311group5/ist311group5/NavigationGUI.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ist311group5.ist311group5;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.paint.Color;
 
-        
-    
-        
 public class NavigationGUI{
     
     private final GridPane rootPane;  
@@ -27,21 +23,37 @@ public class NavigationGUI{
         rootPane.setVgap(10);
         rootPane.setPadding(new Insets(25, 25, 25, 25));
         
-        
         var scene = new Scene(rootPane, 630, 480);
         navStage.setScene(scene);      
         
         Text sceneTitle = new Text("Navigation");
         rootPane.add(sceneTitle, 0, 0, 2, 1);
+        
+        final Text actiontarget = new Text();
+        rootPane.add(actiontarget, 1, 6);
+        
+        Button chatBtn = new Button("Chat");
+        rootPane.add(chatBtn, 1, 2);
+        chatBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.BLUE);
+                actiontarget.setText("Chat button clicked!");           
+            }
+        });
+        
+        Button settingBtn = new Button("Account Settings");
+        rootPane.add(settingBtn, 2, 2);
+        settingBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.GREEN);
+                actiontarget.setText("Account settings button clicked!");           
+            }
+        });
     }
     
     public Pane getRootPane() {
         return rootPane;
     }
-
-
-
-
 }
-
-
